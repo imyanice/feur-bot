ab098d7a40ab3093650a484e4a70547756698300
Yan <yan@yanjobs.me>
2021-07-22 17:42:19 +0200

    :sparkles: Add more words to the list !

--- events/message.js
+++ events/message.js
@@ -7,8 +7,29 @@ module.exports = class InteractionEvent extends BaseEvent {
 
     async run(client, message) {
         if (message.author.bot) return;
-        if (message.content.toLowerCase().endsWith("koi") || message.content.toLowerCase().endsWith("coi") ||message.content.toLowerCase().endsWith("qoi") ||message.content.toLowerCase().endsWith("qwa") || message.content.toLowerCase().endsWith("quoi") || message.content.toLowerCase().endsWith("coi") || message.content.toLowerCase().endsWith("cwa") || message.content.toLowerCase().endsWith("pk") || message.content.toLowerCase().endsWith("pourquoi")) {
-            message.reply("-feur")
+        if (
+               message.content.toLowerCase().endsWith("koi")
+            || message.content.toLowerCase().endsWith("koua")
+            || message.content.toLowerCase().endsWith("kwa")
+            || message.content.toLowerCase().endsWith("coi")
+            || message.content.toLowerCase().endsWith("qoi")
+            || message.content.toLowerCase().endsWith("qwa")
+            || message.content.toLowerCase().endsWith("quoi")
+            || message.content.toLowerCase().endsWith("coi")
+            || message.content.toLowerCase().endsWith("cwa")
+            || message.content.toLowerCase().endsWith("pk")
+            || message.content.toLowerCase().endsWith("pourquoi")
+            || message.content.toLowerCase().endsWith("pourqoi")
+            || message.content.toLowerCase().endsWith("pourcwa")
+            || message.content.toLowerCase().endsWith("pourcoi")
+        ) {
+            message.reply("-feur");
+        } else if (
+               message.content.toLowerCase().endsWith("oui")
+            || message.content.toLowerCase().endsWith("ui")
+            || message.content.toLowerCase().endsWith("wi")
+        ) {
+            message.reply("-stiti")
         }
     }
 };
