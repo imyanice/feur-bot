eb6fc5da49a9766cddd4bbe419ec9d5b7d80a39e
Yan-Jobs <yan@yanjobs.me>
2021-07-23 10:32:56 +0200

    :bulb: Comment the hello command file

--- commands/hello.js
+++ commands/hello.js
@@ -1,10 +1,10 @@
-const BaseCommand = require("../base/BaseCommand");
+const BaseCommand = require("../base/BaseCommand"); // Require the base command file
 
-module.exports = class Hi extends BaseCommand {
+module.exports = class Hi extends BaseCommand { // Create the hi command
     constructor() {
-        super("hello", "fun");
+        super("hello"); // Register the command
     }
     async run(client, interaction) {
-        await interaction.reply("Feur feuuur feeur fffeur ffeeurr ! (Bonjour en feuririen)");
+        await interaction.reply("Feur feuuur feeur fffeur ffeeurr ! (Bonjour en feuririen)"); // Reply with a troll message
     }
 };
