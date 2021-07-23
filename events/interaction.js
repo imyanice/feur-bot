52eb4c99ebfdadf45f164684d6427e2aa2f61497
Yan-Jobs <yan@yanjobs.me>
2021-07-23 10:24:50 +0200

    :bulb: Comment the interaction event file

--- events/interaction.js
+++ events/interaction.js
@@ -1,16 +1,16 @@
-const BaseEvent = require("../base/BaseEvent");
+const BaseEvent = require("../base/BaseEvent"); // Require the base event file
 
-module.exports = class InteractionEvent extends BaseEvent {
+module.exports = class InteractionEvent extends BaseEvent { // Create the interaction event
     constructor() {
-        super("interactionCreate");
+        super("interactionCreate"); // Register the event
     }
 
     async run(client, interaction) {
-        // console.log(interaction) If you want see the JSON object.
-        const command = interaction.commandName;
-        const commandRegistered = client.commands.get(command);
-        if (commandRegistered) {
-            commandRegistered.run(client, interaction);
+        // console.log(interaction) If you want to see the JSON object.
+        const command = interaction.commandName; // Take the command name
+        const commandRegistered = client.commands.get(command); // Get the command from our command map
+        if (commandRegistered) { // If the command was found
+            commandRegistered.run(client, interaction); // Run it
         }
     }
 };
