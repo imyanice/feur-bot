5e96a9363bfee863f34ef07478a527f879944905
Yan-Jobs <yan@yanjobs.me>
2021-07-23 10:30:16 +0200

    :bulb: Comment the message event file

--- events/message.js
+++ events/message.js
@@ -1,13 +1,13 @@
-const BaseEvent = require("../base/BaseEvent");
+const BaseEvent = require("../base/BaseEvent"); // Require the base event file
 
-module.exports = class InteractionEvent extends BaseEvent {
+module.exports = class MessageEvent extends BaseEvent { // Create our message event class
     constructor() {
-        super("messageCreate");
+        super("messageCreate"); // Register our event
     }
 
     async run(client, message) {
-        if (message.author.bot) return;
-        if (
+        if (message.author.bot) return; // If the message is sent by a bot ignore it
+        if ( // Check if the message ends with "quoi" (what in english) (there is also the sms way)
                message.content.toLowerCase().endsWith("koi")
             || message.content.toLowerCase().endsWith("koua")
             || message.content.toLowerCase().endsWith("kwa")
@@ -23,13 +23,13 @@ module.exports = class InteractionEvent extends BaseEvent {
             || message.content.toLowerCase().endsWith("pourcwa")
             || message.content.toLowerCase().endsWith("pourcoi")
         ) {
-            message.reply("-feur");
-        } else if (
+            message.reply("-feur"); // Reply with the fateful message
+        } else if ( // Check if the message ends with "oui" (yes in english) (there is also the sms way)
                message.content.toLowerCase().endsWith("oui")
             || message.content.toLowerCase().endsWith("ui")
             || message.content.toLowerCase().endsWith("wi")
         ) {
-            message.reply("-stiti")
+            message.reply("-stiti") // Reply with the fateful message
         }
     }
 };
