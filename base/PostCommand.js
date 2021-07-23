ab30777c64467b8fec236e100d69d2986891ac19
Yan-Jobs <yan@yanjobs.me>
2021-07-23 10:07:33 +0200

    :fire: Removing unused constant and files

--- base/PostCommand.js
+++
@@ -1,9 +0,0 @@
-module.exports = class PostCommand {
-    constructor(name) {
-        this.name = name;
-    }
-
-    async post(client, data, guild) {
-        await client.guilds.cache.get('867101383152893982')?.commands.create(data);
-    }
-};
