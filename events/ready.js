d9c78e8bafe04829ffc5cc69e9d72907c35af0e0
Yan-Jobs <yan@yanjobs.me>
2021-07-23 10:22:06 +0200

    :bulb: Add comment in the ready event file

--- events/ready.js
+++ events/ready.js
@@ -1,22 +1,22 @@
-const BaseEvent = require("../base/BaseEvent");
+const BaseEvent = require("../base/BaseEvent"); // Require the base event file
 
-module.exports = class ReadyEvent extends BaseEvent {
+module.exports = class ReadyEvent extends BaseEvent { // Create the Ready event
     constructor() {
-        super("ready");
+        super("ready"); // Register the event
     }
 
     async run(client) {
-        client.user.setActivity('Sheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeesh !!!!!')
+        client.user.setActivity('Sheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeesh !!!!!') // Set the status
         console.log(
             `Logged as ${client.user.tag} in ${client.guilds.cache.size} !`
         ); // Console log ready
-        await client.guilds.cache.get('867101383152893982')?.commands.create(
+        await client.guilds.cache.get('867101383152893982')?.commands.create( // Create a base command
             {
                 name: "hello",
                 description: "Reply with Hello because I LOVE U !",
             }
         ).then(() => {
-            console.log("All the commands are successfully posted !");
+            console.log("All the commands are successfully posted !"); // Then say if the commands are posted
         });
     }
 };
