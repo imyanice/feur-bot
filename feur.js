333fa86ed8ef8d2e26a3c50c222417827072300b
Yan-Jobs <yan@yanjobs.me>
2021-07-23 10:52:45 +0200

    :bulb: Comment the feur (index file)

--- feur.js
+++ feur.js
@@ -1,18 +1,18 @@
-const Feur = require("./base/Feur"),
+const Feur = require("./base/Feur"), // Require the base client
     {
-        registerCommands,
-        registerEvents,
+        registerCommands, // Require the register commands function
+        registerEvents, // Require the register events function
     } = require("./utils/handler");
 
-const client = new Feur();
+const client = new Feur(); // Create the client
 
-const init = async () => {
-    await registerEvents(client, "../events");
-    await registerCommands(client, "../commands");
-    await client.login(client.config.token);
+const init = async () => { // Create a init func
+    await registerEvents(client, "../events"); // Execute the register events function
+    await registerCommands(client, "../commands"); // Execute the register commands function
+    await client.login(client.config.token); // Login to the discord api (bot)
 };
 
-init().then(() => console.log("Init terminé"));
+init().then(() => console.log("Init completed !")); // Start the initialisation
 
 
 // If there is an unhandledRejection, log it
