b4c026ac52eab0dd2c5cc8fd7a539834f779d7a4
Yan-Jobs <yan@yanjobs.me>
2021-07-24 10:06:30 +0200

    :sparkles: Add some other way to detect 'quoi'

--- events/message.js
+++ events/message.js
@@ -22,6 +22,21 @@ module.exports = class MessageEvent extends BaseEvent { // Create our message ev
             || message.content.toLowerCase().endsWith("pourqoi")
             || message.content.toLowerCase().endsWith("pourcwa")
             || message.content.toLowerCase().endsWith("pourcoi")
+            || message.content.toLowerCase().endsWith("koi?")
+            || message.content.toLowerCase().endsWith("koua?")
+            || message.content.toLowerCase().endsWith("kwa?")
+            || message.content.toLowerCase().endsWith("coi?")
+            || message.content.toLowerCase().endsWith("qoi?")
+            || message.content.toLowerCase().endsWith("qwa?")
+            || message.content.toLowerCase().endsWith("quoi?")
+            || message.content.toLowerCase().endsWith("coi?")
+            || message.content.toLowerCase().endsWith("cwa?")
+            || message.content.toLowerCase().endsWith("pk?")
+            || message.content.toLowerCase().endsWith("pourquoi?")
+            || message.content.toLowerCase().endsWith("pourqoi?")
+            || message.content.toLowerCase().endsWith("pourcwa?")
+            || message.content.toLowerCase().endsWith("pourcoi?")
+
         ) {
             message.reply("-feur"); // Reply with the fateful message
         } else if ( // Check if the message ends with "oui" (yes in english) (there is also the sms way)
