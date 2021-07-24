03509e072be1f73fe1952fa01c7784ee92e46719
Yan-Jobs <yan@yanjobs.me>
2021-07-24 16:44:08 +0200

    :sparkles: Add the creation of the slash commands for multiple guilds

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -8,5 +8,14 @@ module.exports = class GuildCreateEvent extends BaseEvent {
 
   async run(client, guild) {
     client.db.set(guild.id, "off");
+    for (const data of datas) {
+      await client.guilds.cache
+          .get(guild.id)
+          ?.commands.create(data)
+          .then(() => {
+            console.log(`The command ${data.name} was successfully posted to the ${guild.name} server id: ${guild.id}!`);
+          })
+          .catch((e) => console.log(e));
+    }
   }
 };
